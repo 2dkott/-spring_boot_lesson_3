52a4ff70404b5abc51e9fc90f21eb0afd57d85cb
kot2d <kot2d@outlook.com>
2024-02-08 16:36:13 +0100

    added descripton for controllers

--- src/main/java/com/springboot/lesson3/api/BookController.java
+++ src/main/java/com/springboot/lesson3/api/BookController.java
@@ -1,7 +1,13 @@
 package com.springboot.lesson3.api;
 
 import com.springboot.lesson3.model.Book;
+import com.springboot.lesson3.model.Reader;
 import com.springboot.lesson3.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,6 +25,13 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    @Operation(summary = "Get all Books")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Book list is returned",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Reader.class)) }),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error during Book search",
+                    content = @Content) })
     @GetMapping
     public ResponseEntity<List<Book>> getAll() {
         List<Book> books;
@@ -32,6 +45,12 @@ public class BookController {
         return ResponseEntity.status(HttpStatus.OK).body(books);
     }
 
+    @Operation(summary = "Get a Book by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "302", description = "Book is found",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Book not found",
+                    content = @Content) })
     @GetMapping("/{id}")
     public ResponseEntity<Book> getById(@PathVariable long id) {
         Optional<Book> book;
@@ -44,6 +63,13 @@ public class BookController {
     }
 
 
+    @Operation(summary = "Create a Book")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Book was created",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Reader.class)) }),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error during Book creation",
+                    content = @Content) })
     @PostMapping
     public ResponseEntity<Book> addBook(@RequestBody BookRequest request) {
         Book createdBook;
@@ -57,6 +83,15 @@ public class BookController {
         return ResponseEntity.status(HttpStatus.CREATED).body(createdBook);
     }
 
+    @Operation(summary = "Remove the Book by Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Book was deleted",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Reader.class)) }),
+            @ApiResponse(responseCode = "404", description = "Book not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error during Book removing",
+                    content = @Content) })
     @DeleteMapping("/{id}")
     public ResponseEntity<Book> addBook(@PathVariable long id) {
         Optional<Book> book;
--- src/main/java/com/springboot/lesson3/api/IssuerController.java
+++ src/main/java/com/springboot/lesson3/api/IssuerController.java
@@ -1,10 +1,16 @@
 package com.springboot.lesson3.api;
 
 import com.springboot.lesson3.model.Issue;
+import com.springboot.lesson3.model.Reader;
 import com.springboot.lesson3.service.BookLimitExceededException;
 import com.springboot.lesson3.service.IssuerService;
 import com.springboot.lesson3.service.NoBookException;
 import com.springboot.lesson3.service.NoReaderException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,12 +30,23 @@ public class IssuerController {
   @Autowired
   private IssuerService service;
 
+  @Operation(summary = "Create a Issue")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "201", description = "Issue was created",
+                  content = { @Content(mediaType = "application/json",
+                          schema = @Schema(implementation = Reader.class)) }) })
   @PostMapping
   public ResponseEntity<Issue> issueBook(@RequestBody IssueRequest request) throws NoBookException, BookLimitExceededException, NoReaderException {
     log.info("Получен запрос на выдачу: readerId = {}, bookId = {}", request.getReaderId(), request.getBookId());
     return ResponseEntity.status(HttpStatus.OK).body(service.issue(request));
   }
 
+  @Operation(summary = "Update Issues with return date and time")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "404", description = "Issue not found",
+                  content = @Content),
+          @ApiResponse(responseCode = "302", description = "Reader was found and updated",
+                  content = @Content) })
   @PutMapping()
   public ResponseEntity<Issue> returnBook(@RequestBody IssueRequest request) {
     log.info("Получен запрос на возврат: readerId = {}, bookId = {}", request.getReaderId(), request.getBookId());
